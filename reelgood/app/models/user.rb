7d697dfb8ef25c8bba863ff42c159f60d55cca11
Harrison Andrews <harrisonandrews1986@gmail.com>
2015-11-30 15:58:38 -0500

    fixed model issues, and db:migrate.

---
+++ reelgood/app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  before_validation :ensure_auth_token!
+  validates_presence_of :user_name
+  validates_uniqueness_of :email
+  validates_format_of :email, with: /.+@.+\..+/
+  validates :auth_token, presence: true, uniqueness: true
+
+  has_many :comments
+
+  def ensure_auth_token!
+    if self.auth_token.blank?
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+  end
+
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
+
+
+
